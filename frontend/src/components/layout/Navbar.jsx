5be2c4db9dd3adc49ef0bf8eb7719061c2fc1a07
sahilsheth05 <sahilnsheth@gmail.com>
2025-03-09 12:54:57 -0700

    Completed the navbar frontend. You can use the different icons to click through different pages. Also completed the searchbar in the navbar which will eventually allow you to search for users.

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import Home from "../../icons/home.svg";
+import Logout from "../../icons/logout.svg";
+import Notification from "../../icons/notification.svg";
+import Profile from "../../icons/profile.png";
+import searchIcon from "../../icons/search.svg";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [searchVal, setSearchVal] = useState("");
 
-export default Navbar
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:5501/portfolio/get-stock/${searchVal}`
+      );
+
+      console.log("Price: " + res.data.price);
+    } catch (error) {
+      console.error("Error finding stock: ", error);
+      alert(error.response.data.error);
+    }
+  };
+  return (
+    <div className="navbar">
+      <div className="navbar-center">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setSearchVal("");
+            handleSubmit();
+          }}
+        >
+          <div className="searchBar">
+            <label htmlFor="search" className="icon-search">
+              <img src={searchIcon} alt="Search" />
+            </label>
+            <input
+              id="search"
+              name="search"
+              value={searchVal}
+              placeholder="Search"
+              className="bar"
+              onChange={(e) => {
+                setSearchVal(e.target.value);
+              }}
+            />
+          </div>
+        </form>
+      </div>
+      <div className="navbar-icons">
+        <Link to="/home">
+          <div className="navbar-item">
+            <img src={Home} className="navbar-icon" />
+            <h1 className="navbar-text">Home</h1>
+          </div>
+        </Link>
+
+        <Link to="/notifications">
+          <div className="navbar-item">
+            <img src={Notification} className="navbar-icon" />
+            <h1 className="navbar-text">Notifications</h1>
+          </div>
+        </Link>
+
+        <Link to="/profile">
+          <div className="navbar-item">
+            <img src={Profile} className="navbar-icon" />
+            <h1 className="navbar-text">Profile</h1>
+          </div>
+        </Link>
+
+        <Link to="/login">
+          <div className="navbar-item">
+            <img src={Logout} className="navbar-icon" />
+            <h1 className="navbar-text">Logout</h1>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+//import { Profile } from "../components/Profile.jsx";
+import "./HomePage.css";
+import searchIcon from "../icons/search.svg";
+import axios from "axios";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
 
-export default HomePage
+  return (
+    <div className="homePage">
+      
+    </div>
+  );
+};
+
+export default HomePage;
